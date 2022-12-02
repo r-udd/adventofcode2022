bfe6f360dcb0eccc858b77ad95c73317d6cec6d0
Udd Robert <robert.udd@lm.se>
2022-12-02 14:39:29 +0100

    Touch up

--- day2/test_day2.py
+++ day2/test_day2.py
@@ -2,20 +2,14 @@ def solve_a(rounds):
     results = {'A': {'X': 4, 'Y': 8, 'Z': 3},
                'B': {'X': 1, 'Y': 5, 'Z': 9},
                'C': {'X': 7, 'Y': 2, 'Z': 6}}
-    s = 0
-    for round in rounds:
-        s += results[round[0]][round[1]]
-    return s
+    return sum(map(lambda round: results[round[0]][round[1]], rounds))
 
 
 def solve_b(rounds):
     results = {'A': {'X': 3, 'Y': 4, 'Z': 8},
                'B': {'X': 1, 'Y': 5, 'Z': 9},
                'C': {'X': 2, 'Y': 6, 'Z': 7}}
-    s = 0
-    for round in rounds:
-        s += results[round[0]][round[1]]
-    return s
+    return sum(map(lambda round: results[round[0]][round[1]], rounds))
 
 
 input_a = [['A', 'Y'], ['B', 'X'], ['C', 'Z']]
